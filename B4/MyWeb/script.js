c75d3b30f730c5837be6e92212a49bbe0ceb463e
Max <mcdungkp@gmail.com>
2024-08-07 15:39:31 +0700

    update my repos

---
+++ B4/MyWeb/script.js
@@ -0,0 +1,12 @@
+let isShow = false;
+function display(){
+    
+    if(!isShow){
+
+        document.getElementById("nav").classList.add("show");
+    }
+    else {
+        document.getElementById("nav").classList.remove("show");
+    }
+    isShow = !isShow;
+}
